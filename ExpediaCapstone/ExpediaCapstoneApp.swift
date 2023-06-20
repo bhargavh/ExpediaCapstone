a1eb8ad3fbacc57fe1c581341739d48dc0e46417
Bhargav Hegde <t-bhegde@expediagroup.com>
2023-06-20 17:22:13 +0530

    empty swiftui project

---
+++ ExpediaCapstone/ExpediaCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpediaCapstoneApp.swift
+//  ExpediaCapstone
+//
+//  Created by Bhargav Hegde on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpediaCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
