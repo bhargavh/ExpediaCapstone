ad3b08455584c51d8c67dd77df39e68c03344018
Bhargav Hegde <t-bhegde@expediagroup.com>
2023-06-20 23:05:08 +0530

    Add model

---
+++ ExpediaCapstone/Model/Forecast.swift
@@ -0,0 +1,18 @@
+//
+//  Forecast.swift
+//  ExpediaCapstone
+//
+//  Created by Bhargav Hegde on 20/06/23.
+//
+
+import Foundation
+
+struct Forecast: Hashable, Decodable, Identifiable {
+    var id: Int
+    var temperature: Int
+    var high: Int
+    var low: Int
+    var city: String
+    var country: String
+    var weatherCondition: String
+}
