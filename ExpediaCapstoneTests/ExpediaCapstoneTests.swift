833956ed902f6438125c804f1d97cb8d042c2987
Bhargav Hegde <t-bhegde@expediagroup.com>
2023-06-21 14:46:49 +0530

    Remove force unwrap

--- ExpediaCapstoneTests/ExpediaCapstoneTests.swift
+++ ExpediaCapstoneTests/ExpediaCapstoneTests.swift
@@ -9,39 +9,50 @@ import XCTest
 @testable import ExpediaCapstone // Replace with your app's module name
 
 class ForecastViewModelTests: XCTestCase {
-    var viewModel: ForecastViewModel!
+    var viewModel: ForecastsViewModel? = nil
     
     override func setUpWithError() throws {
-        viewModel = ForecastViewModel()
+        viewModel = ForecastsViewModel()
     }
-
+    
     override func tearDownWithError() throws {
         viewModel = nil
     }
     
     func testPartialSearch(){
         // Test searching by city
-        let cityResults = viewModel.search("Mumb")
+        guard let cityResults = viewModel?.search("Mumb") else {
+            XCTFail()
+            return
+        }
         XCTAssertEqual(cityResults.count, 1)
         XCTAssertEqual(cityResults.first?.city, "Mumbai")
+        
     }
     
     func testEmptySearch(){
         // Test searching with empty query
-        let emptyResults = viewModel.search("")
-        XCTAssertEqual(emptyResults.count, viewModel.forecasts.count)
+        guard let emptyResults = viewModel?.search("") else{
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(emptyResults.count, viewModel?.forecasts.count)
     }
     
     func testSearch() {
         // Test searching by country
-        let countryResults = viewModel.search("India")
+        guard let countryResults = viewModel?.search("India") else{
+            XCTFail()
+            return
+        }
         XCTAssertEqual(countryResults.count, 2)
         XCTAssertTrue(countryResults.contains(where: { $0.country == "India" }))
     }
+    
     func testRefresh(){
-        viewModel.forecasts = []
-        XCTAssertEqual(viewModel.forecasts.count, 0)
-        viewModel.refresh()
-        XCTAssertNotEqual(viewModel.forecasts.count, 0)
+        viewModel?.forecasts = []
+        XCTAssertEqual(viewModel?.forecasts.count, 0)
+        viewModel?.refresh()
+        XCTAssertNotEqual(viewModel?.forecasts.count, 0)
     }
 }
